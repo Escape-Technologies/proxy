c417dd4bac29efe26d8c005c5e278b2464cd65d3
QuentinN42 <quentin@lieumont.fr>
2023-08-16 14:45:10 +0200

    feat: use regex to help user input
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- main.go
+++ main.go
@@ -4,17 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 
 	"github.com/elazarl/goproxy"
 	"github.com/elazarl/goproxy/ext/auth"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
-	username := "admin"
-	password := "admin"
+var UUID = regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`)
+var PORT = regexp.MustCompile(`^[0-9]{1,5}$`)
 
-	start("0.0.0.0", "8080", hash(fmt.Sprintf("%s:%s", username, password)))
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	if ! PORT.MatchString(port) {
+		log.Printf("PORT must be a number between 0 and 65535")
+		os.Exit(1)
+	}
+
+	username := os.Getenv("ESCAPE_ORGANIZATION_ID")
+	if ! UUID.MatchString(username) {
+		log.Printf("ESCAPE_ORGANIZATION_ID must be a UUID in lowercase")
+		log.Printf("To get your organization id, go to https://app.escape.tech/organization/settings/")
+		os.Exit(1)
+	}
+	password := os.Getenv("ESCAPE_API_KEY")
+	if ! UUID.MatchString(password) {
+		log.Printf("ESCAPE_API_KEY must be a UUID in lowercase")
+		log.Printf("To get your API key, go to https://app.escape.tech/user/profile/")
+		os.Exit(1)
+	}
+
+	start("0.0.0.0", port, hash(username + password))
 }
 
 func hash(data string) []byte {
@@ -39,7 +63,7 @@ func start(addr string, port string, expected_hash []byte) {
 	proxy := goproxy.NewProxyHttpServer()
 
 	auth.ProxyBasic(proxy, "realm", func(user, pass string) bool {
-		return compare(expected_hash, fmt.Sprintf("%s:%s", user, pass))
+		return compare(expected_hash, user + pass)
 	})
 
 	log.Printf("Listening on %s", total)
