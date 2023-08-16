b34d1b22dd7c52249400e844ecb059c58b074f8f
QuentinN42 <quentin@lieumont.fr>
2023-08-16 14:23:55 +0200

    feat: use bcrypt to resist timing attack
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21
 require (
 	github.com/elazarl/goproxy v0.0.0-20190711103511-473e67f1d7d2
 	github.com/elazarl/goproxy/ext v0.0.0-20190711103511-473e67f1d7d2
+	golang.org/x/crypto v0.12.0
 )
--- main.go
+++ main.go
@@ -7,18 +7,39 @@ import (
 
 	"github.com/elazarl/goproxy"
 	"github.com/elazarl/goproxy/ext/auth"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	start("0.0.0.0", "8080", "user", "test")
+	username := "admin"
+	password := "admin"
+
+	start("0.0.0.0", "8080", hash(fmt.Sprintf("%s:%s", username, password)))
 }
 
-func start(addr string, port string, user string, password string) {
+func hash(data string) []byte {
+	bytes := []byte(data)
+	hash, err := bcrypt.GenerateFromPassword(bytes, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return hash
+}
+
+func compare(hash []byte, data string) bool {
+	err := bcrypt.CompareHashAndPassword(hash, []byte(data))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func start(addr string, port string, expected_hash []byte) {
 	total := fmt.Sprintf("%s:%s", addr, port)
 	proxy := goproxy.NewProxyHttpServer()
 
-	auth.ProxyBasic(proxy, "realm", func(user, pwd string) bool {
-		return user == "user" && password == pwd
+	auth.ProxyBasic(proxy, "realm", func(user, pass string) bool {
+		return compare(expected_hash, fmt.Sprintf("%s:%s", user, pass))
 	})
 
 	log.Printf("Listening on %s", total)
